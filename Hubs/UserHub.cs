601bc22458030b3051d2dcdce2b70f455ce1d34e
NghiNV <nghinv03@gmail.com>
2024-12-17 16:26:36 +0700

    count total views and total users

---
+++ Hubs/UserHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo.Hubs
+{
+	public class UserHub : Hub
+	{
+		public static int TotalViews { get; set; } = 0;
+		public static int TotalUsers { get; set; } = 0;
+
+		public override Task OnConnectedAsync()
+		{
+			// When connect then toal users increase by one
+			++TotalUsers;
+			Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter();
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception? exception)
+		{
+			// When disconnect then toal users increase by one
+			--TotalUsers;
+			Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter();
+			return base.OnDisconnectedAsync(exception);
+		}
+
+		public async Task NewWindowLoaded()
+		{
+			++TotalViews;
+			// Send update to all clients that total views have been updated
+			await Clients.All.SendAsync("updateTotalViews", TotalViews);
+		}
+
+	}
+}
---
+++ wwwroot/js/usersCount.js
@@ -0,0 +1,31 @@
+﻿// Create connection
+const connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build()
+
+// Connection method that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+	const newCountSpan = document.getElementById("totalViewsCounter")
+	newCountSpan.innerText = value.toString()
+})
+
+connectionUserCount.on("updateTotalUsers", value => {
+	const newCountSpan = document.getElementById("totalUsersCounter")
+	newCountSpan.innerText = value.toString()
+})
+
+// Invoke hub methods aka send notifications to hub
+function newWindowLoadClient() {
+	connectionUserCount.send("NewWindowLoaded")
+}
+
+// Start connection
+function fulfilled() {
+	// do something on start
+	console.log("Connection to User hub successful")
+	newWindowLoadClient();
+}
+function rejected() {
+	// rejected logs
+	console.error("Connection to User hub fail")
+}
+
+connectionUserCount.start().then(fulfilled, rejected)
